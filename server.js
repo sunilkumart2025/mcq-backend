7fc236acaf2db5c3a167b8d55126badb444d7523
sunilkumar_25 <suniltamil2025@gmail.com>
2025-04-05 00:27:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
+app.use(cors());
 const { Pool } = require("pg");
 const nodemailer = require("nodemailer");
 
