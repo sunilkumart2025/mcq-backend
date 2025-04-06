fb149919a644afdc1bb2c8303198842240408ae5
sunilkumar_25 <suniltamil2025@gmail.com>
2025-04-06 14:45:23 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,12 +1,16 @@
-const mysql = require('mysql2');
-const connection = mysql.createConnection({
+// db.js or inside server.js if combined
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const pool = new Pool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  password: process.env.DB_PASS,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
-connection.connect(err => {
-  if (err) throw err;
-  console.log("✅ Connected to MySQL Database");
-});
-module.exports = connection;
+
+module.exports = pool;
